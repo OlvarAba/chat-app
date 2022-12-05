1247dd2fe15dd0cd24ce2b9439441f0304ea632d
Olvar Abadeen <oabadeen@gmail.com>
2022-12-05 15:50:40 -0800

    5.3 added firebase

--- components/Chat.js
+++ components/Chat.js
@@ -1,48 +1,107 @@
-import React, { Component } from "react";
-import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import React from "react";
+import { View, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { GiftedChat } from "react-native-gifted-chat";
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
-    }
+      uid: 0,
+      user: {
+        _id: '',
+        avatar: '',
+        name: '',
+      }
+    };
+
+    const firebaseConfig = {
+  apiKey: "AIzaSyBzovDgminALHPBLlqWDZ8wTV8gN4Be5Ag",
+  authDomain: "chatapp-8f296.firebaseapp.com",
+  projectId: "chatapp-8f296",
+  storageBucket: "chatapp-8f296.appspot.com",
+  messagingSenderId: "650424371015",
+  appId: "1:650424371015:web:ea5a4027120c9c031e2e62",
+  measurementId: "G-RXWED806KK"
+};
+if (!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+  }
+  this.referenceChatMessages = firebase.firestore().collection("messages");
   }
   
   componentDidMount() {
     const name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+     if (!user) {
+       firebase.auth().signInAnonymously();
+     }
+     this.setState({
+       uid: user.uid,
+       messages: [],
+     });
+     this.referenceChatMessages = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
+     this.unsubscribe = this.referenceChatMessages
+       .orderBy("createdAt", "desc")
+       .onSnapshot(this.onCollectionUpdate);
+   });
+    this.unsubscribe = this.referenceChatMessages.onSnapshot(this.collectionUpdate);
+  }
 
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 2,
-          text: "You Have Entered Chat",
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
+  addMessages = (message) => {
+
+        this.referenceChatMessages.add({
+            _id: message._id,
+            text: message.text,
+            createdAt: message.createdAt,
+            user: message.user,
+        });
+    }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get query document snapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
     });
   }
 
-  
+  addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  };
 
  onSend(messages = []) {
-   this.setState(previousState => ({
-     messages: GiftedChat.append(previousState.messages, messages),
-   }))
- }
+  this.setState((previousState) => ({
+    messages: GiftedChat.append(previousState.messages, messages),
+  }),() => {
+    this.addMessages();
+    this.saveMessages();
+  });
+}
 
  renderBubble(props) {
   return (
@@ -78,6 +137,17 @@ export default class Chat extends React.Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   chatContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  item: {
+    fontSize: 20,
+    color: 'blue',
+  },
+  text: {
+    fontSize: 30,
+  }
+});
